9bf9f5c81aad5e73f72d70ae0502462eca13f1a3
cutewonny <devwonny@gmail.com>
2024-03-06 21:21:52 +0900

    Initial commit

---
+++ src/main/java/com/example/googleExcel/GoogleExcelApplication.java
@@ -0,0 +1,13 @@
+package com.example.googleExcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleExcelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/googleExcel/GoogleExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.googleExcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
